b3f1b82c2fb663a7fece9f6c08113c7be7521633
GuillermoReyes <guillermo@Guillermos-MacBook-Pro.local>
2017-02-23 23:56:59 -0800

    added registration page

--- GymApp/app/src/main/java/com/theteamshetoldyounottoworryabout/gymapp/LoginRegister.java
+++ GymApp/app/src/main/java/com/theteamshetoldyounottoworryabout/gymapp/LoginRegister.java
@@ -2,6 +2,9 @@ package com.theteamshetoldyounottoworryabout.gymapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class LoginRegister extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class LoginRegister extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_register);
+
+        //gather text from fields
+        final EditText etName = (EditText) findViewById(R.id.etName);
+        final EditText Weight = (EditText) findViewById(R.id.Weight);
+        final EditText PhoneNum = (EditText) findViewById(R.id.PhoneNum);
+        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+
+        //register button
+        final Button bRegister = (Button) findViewById(R.id.bRegister);
+
+
+
+
     }
 }
