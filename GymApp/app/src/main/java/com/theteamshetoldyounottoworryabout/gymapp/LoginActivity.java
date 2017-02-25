e54fedfa71aafea014055f3d1d10bd3b2d0aa76b
GuillermoReyes <guillermo@Guillermos-MacBook-Pro.local>
2017-02-25 12:08:24 -0800

    added login/register/user-activity interface

--- GymApp/app/src/main/java/com/theteamshetoldyounottoworryabout/gymapp/LoginActivity.java
+++ GymApp/app/src/main/java/com/theteamshetoldyounottoworryabout/gymapp/LoginActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,17 +18,17 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         final EditText m_login = (EditText) findViewById(R.id.m_login);
-        final EditText Password = (EditText) findViewById(R.id.password);
+        final EditText Password = (EditText) findViewById(R.id.etPassword);
         final Button bLogin = (Button) findViewById(R.id.button);
         final TextView registerLink = (TextView) findViewById(R.id.m_registerHere);
 
-        registerLink.setOnClickListener(new View.OnClickListener() {
-            @override
-            public void onClickView(View v){
+        registerLink.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
                 Intent registerIntent = new Intent(LoginActivity.this,LoginRegister.class);
-                LoginRegister.this.startActivity(registerIntent);
+                LoginActivity.this.startActivity(registerIntent);
             }
-
         });
+
     }
 }
