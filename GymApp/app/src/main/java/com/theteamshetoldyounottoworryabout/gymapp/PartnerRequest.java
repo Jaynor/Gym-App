488bcb9c1faea89858633932e925122669d4eaa3
GuillermoReyes <guillermo@Guillermos-MacBook-Pro.local>
2017-03-01 22:10:24 -0800

    added new modules

---
+++ GymApp/app/src/main/java/com/theteamshetoldyounottoworryabout/gymapp/PartnerRequest.java
@@ -0,0 +1,8 @@
+package com.theteamshetoldyounottoworryabout.gymapp;
+
+/**
+ * Created by guillermo on 3/1/17.
+ */
+//TODO: add functionality to scan for a partner
+public class PartnerRequest {
+}
