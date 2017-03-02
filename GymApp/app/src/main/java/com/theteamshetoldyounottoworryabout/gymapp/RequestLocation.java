aa6ae38385c18bb293fc8024d04a6ccf656a9732
GuillermoReyes <guillermo@Guillermos-MacBook-Pro.local>
2017-03-02 11:20:18 -0800

    permission to access location
    added LocationRequest class

---
+++ GymApp/app/src/main/java/com/theteamshetoldyounottoworryabout/gymapp/RequestLocation.java
@@ -0,0 +1,19 @@
+package com.theteamshetoldyounottoworryabout.gymapp;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by guillermo on 3/2/17.
+ */
+
+public class RequestLocation extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        //create an instance of google [play location API
+    }
+}
