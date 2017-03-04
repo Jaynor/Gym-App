46d0d0c7a6e663ede980b873056c6f5369ed8253
GuillermoReyes <guillermo@Guillermos-MacBook-Pro.local>
2017-03-04 10:30:17 -0800

    added Request Partner module

--- GymApp/app/src/main/java/com/theteamshetoldyounottoworryabout/gymapp/UserAreaActivity.java
+++ GymApp/app/src/main/java/com/theteamshetoldyounottoworryabout/gymapp/UserAreaActivity.java
@@ -3,6 +3,8 @@ package com.theteamshetoldyounottoworryabout.gymapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -30,9 +32,15 @@ public class UserAreaActivity extends AppCompatActivity {
         etMessage.setText(message);
         etName.setText(name);
 
+        final Button bFindPartner  = (Button)findViewById(R.id.etRequestPartner);
 
-
-
+        //create a button listener
+        bFindPartner.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //perform action when clicking on the object
+            }
+        });
 
 
 
