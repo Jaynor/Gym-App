f4ca81693ea5c16ef538a2ba94f17cd720b54da0
GuillermoReyes <guillermo@Guillermos-MacBook-Pro.local>
2017-03-03 20:54:10 -0800

    added google play location API
    We can now start making our app location-aware!!!

--- GymApp/app/src/main/java/com/theteamshetoldyounottoworryabout/gymapp/RequestLocation.java
+++ GymApp/app/src/main/java/com/theteamshetoldyounottoworryabout/gymapp/RequestLocation.java
@@ -2,18 +2,77 @@ package com.theteamshetoldyounottoworryabout.gymapp;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.content.IntentSender;
+import android.location.Location;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * Created by guillermo on 3/2/17.
  */
 
-public class RequestLocation extends AppCompatActivity{
+public class RequestLocation extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks,
+    GoogleApiClient.OnConnectionFailedListener, LocationListener{
 
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
 
         //create an instance of google [play location API
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+    private void setUpMapIfNeeded(){
+
+    }
+    private void setUpMap(){
+
+
+    }
+    private void handleNewLocation(Location location){
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
     }
 }
